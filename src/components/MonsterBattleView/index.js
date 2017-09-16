d4e4da241cdf14d045a61700ddaa1aa1b70f1bf9
eL0ck <tpj800@gmail.com>
2017-09-16 11:13:27 +1000

    Monster Game: 1: Adding names to the monsters

--- src/components/MonsterBattleView/index.js
+++ src/components/MonsterBattleView/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 import MonsterAvatar from '../MonsterAvatar'
+import MonsterName from '../MonsterName'
 
 import './styles.css'
 
 const MonsterBattleView = ({name, currentHealth, flip, winner}) => (
   <div className={'MonsterBattleView'}>
     <MonsterAvatar name={name} flip={flip} dead={currentHealth <= 0} />
-
+    <MonsterName name={name} />
     {winner && <div className={'MonsterBattleView__fireworks'} />}
   </div>
 )
