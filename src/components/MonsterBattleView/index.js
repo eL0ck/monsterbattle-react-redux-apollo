1cbedb2651e5d4bde904372de549623cd9dda2c5
eL0ck <tpj800@gmail.com>
2017-09-16 12:00:33 +1000

    Monster Game: 2: Implimented monster game stats

--- src/components/MonsterBattleView/index.js
+++ src/components/MonsterBattleView/index.js
@@ -2,13 +2,26 @@ import React from 'react'
 
 import MonsterAvatar from '../MonsterAvatar'
 import MonsterName from '../MonsterName'
+import MonsterHealth from '../MonsterHealth'
+import MonsterStats from '../MonsterStats'
 
 import './styles.css'
 
-const MonsterBattleView = ({name, currentHealth, flip, winner}) => (
+const MonsterBattleView = ({
+  name,
+  health,
+  currentHealth,
+  flip,
+  winner,
+  attack,
+  defence
+}) => (
   <div className={'MonsterBattleView'}>
     <MonsterAvatar name={name} flip={flip} dead={currentHealth <= 0} />
-    <MonsterName name={name} />
+    <MonsterHealth health={health} currentHealth={currentHealth} flip={flip}>
+      <MonsterName name={name} />
+    </MonsterHealth>
+    <MonsterStats attack={attack} defence={defence} health={health}/>
     {winner && <div className={'MonsterBattleView__fireworks'} />}
   </div>
 )
--- src/components/MonsterHealth/index.js
+++ src/components/MonsterHealth/index.js
@@ -1,1 +1,23 @@
-export default () => false
+import React from 'react'
+import cx from 'classnames'
+
+import './styles.css';
+
+function MonsterHealth({
+  health,
+  currentHealth,
+  flip,
+  children }) {
+  return <div className={cx('MonsterHealth', {'MonsterHealth--flip': flip})}>
+    <div className="MonsterHealth__bar" style={{
+      width: (Math.min(currentHealth/health, 1)*100) + '%'
+      }}>
+      {children && <div className='MonsterHealth__info'>
+        {children}
+        </div>
+      }
+    </div>
+  </div>
+}
+
+export default MonsterHealth
