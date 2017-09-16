04dbab3768c3a7bfcc917a388a2e1cc354e6362c
eL0ck <tpj800@gmail.com>
2017-09-16 12:27:11 +1000

    Monster Game: 2b: Implimented more monster game stats ??

--- src/components/MonsterStats/index.js
+++ src/components/MonsterStats/index.js
@@ -1,1 +1,22 @@
-export default () => false
+import React from 'react'
+
+import './styles.css'
+
+function MonsterStats({attack, defence, health}){
+  return <div className='MonsterStats'>
+    <div className='MonsterStats__attack'>
+      <span className='MonsterStats__label'>Attack:</span>
+      <span className='MonsterStats__value'>{attack}</span>
+    </div>
+    <div className='MonsterStats__defence'>
+      <span className='MonsterStats__label'>Defence:</span>
+      <span className='MonsterStats__value'>{defence}</span>
+    </div>
+    <div className='MonsterStats__health'>
+      <span className='MonsterStats__label'>Health:</span>
+      <span className='MonsterStats__value'>{health}</span>
+    </div>
+  </div>
+}
+
+export default MonsterStats
