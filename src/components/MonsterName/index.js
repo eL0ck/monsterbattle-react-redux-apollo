728b3feb6c0a5be2d873006c3496475c8726ee93
eL0ck <tpj800@gmail.com>
2017-09-16 11:15:49 +1000

    Monster Game: 1a: function alternative to arrow

--- src/components/MonsterName/index.js
+++ src/components/MonsterName/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 import './styles.css';
 
-const MonsterName = ({name}) =>
-  <div className={'MonsterName'}>{name}</div>
+function MonsterName ({name}) {
+  return <div className={'MonsterName'}>{name}</div>
+}
 
 export default MonsterName
 
